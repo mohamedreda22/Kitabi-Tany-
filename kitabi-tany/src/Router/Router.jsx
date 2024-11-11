85e84766aed5d701f211c22decdea604817ce11a
= <=>
2024-11-11 02:17:31 +0200

    Refactor  :  CSS and update Google Font link for improved styling

--- kitabi-tany/src/Router/Router.jsx
+++ kitabi-tany/src/Router/Router.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as AppRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from 'react';
-import Register from "../Pages/Register.js";
-import Login from "../Pages/Login.js";
+
 
 const Home = lazy(() => import("../Pages/Home/Home"));
+const Login = lazy(() => import("../Pages/Login.js"));
+const Register = lazy(() => import("../Pages/Register.js"));
 
 const Router = () => {
     return (
@@ -13,12 +14,9 @@ const Router = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/home" element={<Home />} />
                     {/* <Route path="/profile" element={<Profile />} /> */}
                     {/* <Route path="/book/:id" element={<BookDetails />} /> */}
                     {/* <Route path="/add-book" element={<AddBook />} /> */}
-                    
-
                 </Routes>
             </Suspense>
         </AppRouter>
