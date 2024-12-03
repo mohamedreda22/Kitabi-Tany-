14a5430dabc8a6f7687607f3f50e207f4d24242d
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2024-12-03 19:48:54 +0200

    npm install react-icons

--- kitabi-tany/src/Component/App/App.jsx
+++ kitabi-tany/src/Component/App/App.jsx
@@ -1,12 +1,9 @@
 import Router from "../../Router/Router";
-import Header from "../Shared/Header.js";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router />
-    </div>
+    
+    <Router />
   );
 }
 
